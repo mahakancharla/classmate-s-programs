13619d5e9507e81877b6b7494b5463342427a1bd
mahakancharla <159503143+mahakancharla@users.noreply.github.com>
2024-09-24 09:51:53 -0400

    Create thing.cpp

---
+++ thing.cpp
@@ -0,0 +1,11 @@
+print("What's the weather like? (Enter your input as integers")
+print("1. Rainy")
+print("2. Hot")
+print("3. Freezing") 
+weather  = input()
+if (input == 1):
+  print("Use an umbrella")
+elif (input == 2):
+  print("Wear shorts")
+else:
+  print("Wear a snow jacket")
